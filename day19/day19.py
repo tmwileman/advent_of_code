2707b64861e0a540061624fa73cc96a558599267
twileman <twileman@redhat.com>
2020-11-05 21:25:31 -0500

    day19 and refactor directory

---
+++ day19/day19.py
@@ -0,0 +1,338 @@
+"""
+Unsure of the state of Santa's ship, you borrowed the tractor beam technology from Triton. Time to test it out.
+
+When you're safely away from anything else, you activate the tractor beam, but nothing happens. It's hard to tell whether it's working if there's nothing to use it on. Fortunately, your ship's drone system can be configured to deploy a drone to specific coordinates and then check whether it's being pulled. There's even an Intcode program (your puzzle input) that gives you access to the drone system.
+
+The program uses two input instructions to request the X and Y position to which the drone should be deployed. Negative numbers are invalid and will confuse the drone; all numbers should be zero or positive.
+
+Then, the program will output whether the drone is stationary (0) or being pulled by something (1). For example, the coordinate X=0, Y=0 is directly in front of the tractor beam emitter, so the drone control program will always report 1 at that location.
+
+To better understand the tractor beam, it is important to get a good picture of the beam itself. For example, suppose you scan the 10x10 grid of points closest to the emitter:
+
+       X
+  0->      9
+ 0#.........
+ |.#........
+ v..##......
+  ...###....
+  ....###...
+Y .....####.
+  ......####
+  ......####
+  .......###
+ 9........##
+
+In this example, the number of points affected by the tractor beam in the 10x10 area closest to the emitter is 27.
+
+However, you'll need to scan a larger area to understand the shape of the beam. How many points are affected by the tractor beam in the 50x50 area closest to the emitter? (For each of X and Y, this will be 0 through 49.)
+"""
+
+from typing import List, NamedTuple, Tuple, Iterable, Set, Dict, Callable
+from enum import Enum
+import itertools
+from collections import deque, defaultdict
+import logging
+import copy
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+class Opcode(Enum):
+    ADD = 1
+    MULTIPLY = 2
+    STORE_INPUT = 3
+    SEND_TO_OUTPUT = 4
+    JUMP_IF_TRUE = 5
+    JUMP_IF_FALSE = 6
+    LESS_THAN = 7
+    EQUALS = 8
+    ADJUST_RELATIVE_BASE = 9
+
+    END_PROGRAM = 99
+
+
+Modes = List[int]
+Program = List[int]
+
+
+class EndProgram(Exception): pass
+
+
+def parse_opcode(opcode: int, num_modes: int = 3) -> Tuple[Opcode, Modes]:
+    # logging.debug(f"parsing {opcode}")
+
+    opcode_part = opcode % 100
+
+    modes: List[int] = []
+    opcode = opcode // 100
+
+    for _ in range(num_modes):
+        modes.append(opcode % 10)
+        opcode = opcode // 10
+
+    return Opcode(opcode_part), modes
+
+
+class IntcodeComputer:
+    def __init__(self, program: List[int], get_input: Callable[[], int]) -> None:
+        self.program = defaultdict(int)
+        self.program.update({i: value for i, value in enumerate(program)})
+        self.get_input = get_input
+        self.pos = 0
+        self.relative_base = 0
+
+    def save(self):
+        return [
+            copy.deepcopy(self.program),
+            self.get_input,
+            self.pos,
+            self.relative_base
+        ]
+
+    @staticmethod
+    def load(program, get_input, pos, relative_base):
+        computer = IntcodeComputer([], get_input)
+        computer.program = program
+        computer.pos = pos
+        computer.relative_base = relative_base
+        return computer
+
+    def _get_value(self, pos: int, mode: int) -> int:
+        if mode == 0:
+            # pointer mode
+            # logging.debug(f"pos: {pos}, mode: {mode}, value: {self.program[self.program[pos]]}")
+            return self.program[self.program[pos]]
+        elif mode == 1:
+            # immediate mode
+            # logging.debug(f"pos: {pos}, mode: {mode}, value: {self.program[pos]}")
+            return self.program[pos]
+        elif mode == 2:
+            # relative mode
+            # logging.debug(f"pos: {pos}, mode: {mode}, value: {self.program[self.program[pos] + self.relative_base]}")
+            return self.program[self.program[pos] + self.relative_base]
+        else:
+            raise ValueError(f"unknown mode: {mode}")
+
+    def _loc(self, pos: int, mode: int) -> int:
+        if mode == 0:
+            # pointer mode
+            # logging.debug(f"pos: {pos}, mode: {mode}, value: {self.program[pos]}")
+            return self.program[pos]
+        elif mode == 2:
+            # relative mode
+            # logging.debug(f"pos: {pos}, mode: {mode}, value: {self.program[pos] + self.relative_base}")
+            return self.program[pos] + self.relative_base
+
+    def go(self) -> int:
+
+        while True:
+            # logging.debug(f"program: {self.program}")
+            # logging.debug(f"pos: {self.pos}, inputs: {self.inputs}, relative_base: {self.relative_base}")
+
+            opcode, modes = parse_opcode(self.program[self.pos])
+
+            # logging.debug(f"opcode: {opcode}, modes: {modes}")
+
+            if opcode == Opcode.END_PROGRAM:
+                raise EndProgram
+
+            elif opcode == Opcode.ADD:
+                value1 = self._get_value(self.pos + 1, modes[0])
+                value2 = self._get_value(self.pos + 2, modes[1])
+                loc = self._loc(self.pos + 3, modes[2])
+
+                # logging.debug(f"value1: {value1}, value2: {value2}, loc: {loc}")
+
+                self.program[loc] = value1 + value2
+                self.pos += 4
+
+            elif opcode == Opcode.MULTIPLY:
+                value1 = self._get_value(self.pos + 1, modes[0])
+                value2 = self._get_value(self.pos + 2, modes[1])
+                loc = self._loc(self.pos + 3, modes[2])
+
+                # logging.debug(f"value1: {value1}, value2: {value2}, loc: {loc}")
+
+                self.program[loc] = value1 * value2
+                self.pos += 4
+
+            elif opcode == Opcode.STORE_INPUT:
+                # Get input and store at location
+                loc = self._loc(self.pos + 1, modes[0])
+                input_value = self.get_input()
+                self.program[loc] = input_value
+                self.pos += 2
+
+            elif opcode == Opcode.SEND_TO_OUTPUT:
+                # Get output from location
+                value = self._get_value(self.pos + 1, modes[0])
+                self.pos += 2
+                # logging.debug(f"output: {value}")
+
+                ####
+                ####
+
+                return value
+
+            elif opcode == Opcode.JUMP_IF_TRUE:
+                # jump if true
+                value1 = self._get_value(self.pos + 1, modes[0])
+                value2 = self._get_value(self.pos + 2, modes[1])
+
+                # logging.debug(f"value1: {value1}, value2: {value2}")
+
+                if value1 != 0:
+                    self.pos = value2
+                else:
+                    self.pos += 3
+
+            elif opcode == Opcode.JUMP_IF_FALSE:
+                value1 = self._get_value(self.pos + 1, modes[0])
+                value2 = self._get_value(self.pos + 2, modes[1])
+
+                # logging.debug(f"value1: {value1}, value2: {value2}")
+
+                if value1 == 0:
+                    self.pos = value2
+                else:
+                    self.pos += 3
+
+            elif opcode == Opcode.LESS_THAN:
+                value1 = self._get_value(self.pos + 1, modes[0])
+                value2 = self._get_value(self.pos + 2, modes[1])
+                loc = self._loc(self.pos + 3, modes[2])
+
+                # logging.debug(f"value1: {value1}, value2: {value2}, loc: {loc}")
+
+                if value1 < value2:
+                    self.program[loc] = 1
+                else:
+                    self.program[loc] = 0
+                self.pos += 4
+
+            elif opcode == Opcode.EQUALS:
+                value1 = self._get_value(self.pos + 1, modes[0])
+                value2 = self._get_value(self.pos + 2, modes[1])
+                loc = self._loc(self.pos + 3, modes[2])
+
+                # logging.debug(f"value1: {value1}, value2: {value2}, loc: {loc}")
+
+                if value1 == value2:
+                    self.program[loc] = 1
+                else:
+                    self.program[loc] = 0
+                self.pos += 4
+
+            elif opcode == Opcode.ADJUST_RELATIVE_BASE:
+                value = self._get_value(self.pos + 1, modes[0])
+
+                # logging.debug(f"value: {value}")
+
+                self.relative_base += value
+                self.pos += 2
+
+            else:
+                raise ValueError(f"invalid opcode: {opcode}")
+
+PROGRAM = [109,424,203,1,21101,0,11,0,1105,1,282,21101,0,18,0,1106,0,259,2102,1,1,221,203,1,21102,31,1,0,1105,1,282,21101,38,0,0,1105,1,259,21001,23,0,2,21201,1,0,3,21101,0,1,1,21101,0,57,0,1106,0,303,1202,1,1,222,20102,1,221,3,21002,221,1,2,21101,259,0,1,21102,80,1,0,1106,0,225,21101,0,189,2,21102,91,1,0,1105,1,303,2102,1,1,223,20101,0,222,4,21102,259,1,3,21101,225,0,2,21102,225,1,1,21102,1,118,0,1105,1,225,21001,222,0,3,21102,1,57,2,21102,1,133,0,1106,0,303,21202,1,-1,1,22001,223,1,1,21102,148,1,0,1106,0,259,1202,1,1,223,21001,221,0,4,20101,0,222,3,21101,0,24,2,1001,132,-2,224,1002,224,2,224,1001,224,3,224,1002,132,-1,132,1,224,132,224,21001,224,1,1,21101,195,0,0,106,0,108,20207,1,223,2,20102,1,23,1,21102,-1,1,3,21101,0,214,0,1106,0,303,22101,1,1,1,204,1,99,0,0,0,0,109,5,1201,-4,0,249,22101,0,-3,1,22101,0,-2,2,22102,1,-1,3,21102,250,1,0,1106,0,225,22101,0,1,-4,109,-5,2106,0,0,109,3,22107,0,-2,-1,21202,-1,2,-1,21201,-1,-1,-1,22202,-1,-2,-2,109,-3,2106,0,0,109,3,21207,-2,0,-1,1206,-1,294,104,0,99,21201,-2,0,-2,109,-3,2105,1,0,109,5,22207,-3,-4,-1,1206,-1,346,22201,-4,-3,-4,21202,-3,-1,-1,22201,-4,-1,2,21202,2,-1,-1,22201,-4,-1,1,21201,-2,0,3,21102,343,1,0,1105,1,303,1105,1,415,22207,-2,-3,-1,1206,-1,387,22201,-3,-2,-3,21202,-2,-1,-1,22201,-3,-1,3,21202,3,-1,-1,22201,-3,-1,2,21201,-4,0,1,21101,384,0,0,1106,0,303,1106,0,415,21202,-4,-1,-4,22201,-4,-3,-4,22202,-3,-2,-2,22202,-2,-4,-4,22202,-3,-2,-3,21202,-4,-1,-2,22201,-3,-2,1,22102,1,1,-4,109,-5,2105,1,0]
+
+total = 0 
+
+#for i in range(50):
+#    for j in range(50):
+#        inputs = [i, j]
+#        it = iter(inputs)
+#        get_input = lambda: next(it)
+#        computer = IntcodeComputer(PROGRAM, lambda: next(it))
+#        total += computer.go()
+
+#print(total)
+
+
+"""
+You aren't sure how large Santa's ship is. You aren't even sure if you'll need to use this thing on Santa's ship, but it doesn't hurt to be prepared. You figure Santa's ship might fit in a 100x100 square.
+
+The beam gets wider as it travels away from the emitter; you'll need to be a minimum distance away to fit a square of that size into the beam fully. (Don't rotate the square; it should be aligned to the same axes as the drone grid.)
+
+For example, suppose you have the following tractor beam readings:
+
+#.......................................
+.#......................................
+..##....................................
+...###..................................
+....###.................................
+.....####...............................
+......#####.............................
+......######............................
+.......#######..........................
+........########........................
+.........#########......................
+..........#########.....................
+...........##########...................
+...........############.................
+............############................
+.............#############..............
+..............##############............
+...............###############..........
+................###############.........
+................#################.......
+.................########OOOOOOOOOO.....
+..................#######OOOOOOOOOO#....
+...................######OOOOOOOOOO###..
+....................#####OOOOOOOOOO#####
+.....................####OOOOOOOOOO#####
+.....................####OOOOOOOOOO#####
+......................###OOOOOOOOOO#####
+.......................##OOOOOOOOOO#####
+........................#OOOOOOOOOO#####
+.........................OOOOOOOOOO#####
+..........................##############
+..........................##############
+...........................#############
+............................############
+.............................###########
+
+In this example, the 10x10 square closest to the emitter that fits entirely within the tractor beam has been marked O. Within it, the point closest to the emitter (the only highlighted O) is at X=25, Y=20.
+
+Find the 100x100 square closest to the emitter that fits entirely within the tractor beam; within that square, find the point closest to the emitter. What value do you get if you take that point's X coordinate, multiply it by 10000, then add the point's Y coordinate? (In the example above, this would be 250020.)
+"""
+
+def check(i: int, j: int) -> int:
+    inputs = [i, j]
+    it = iter(inputs)
+    get_input = lambda: next(it)
+    computer = IntcodeComputer(PROGRAM, get_input)
+    return computer.go()
+
+hilo = {}
+
+i = 6
+jlo = 5
+jhi = 5    
+
+results = {}
+
+for i in range(6, 2000):
+    while not check(i, jlo):
+        jlo += 1
+    jhi = max(jhi, jlo)
+    while check(i, jhi):
+        jhi += 1
+    jhi -= 1
+
+    results[i] = (jlo, jhi)
+
+    if (i - 99) in results:
+        klo, khi = results[i - 99]
+        if khi >= jlo + 99:
+            top_left = (i - 99, jlo)
+
+            print(top_left[1] * 10_000 + top_left[0])
+            break
+
+    print(i, jlo, jhi)
+
+
+
+
