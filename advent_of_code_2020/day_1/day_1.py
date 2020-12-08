819522130e83f6623c1445a360ba2184abacb39f
twileman <twileman@redhat.com>
2020-12-07 22:41:21 -0500

    Added comments to day 1

--- advent_of_code_2020/day_1/day_1.py
+++ advent_of_code_2020/day_1/day_1.py
@@ -7,8 +7,10 @@ def find_product(inputs: List[int]) -> int:
     Find the two elements that add up to 2020 and return their product
     """
 
+    # Create a set of the difference between 2020 and each input.
     needs = {2020 - i for i in inputs}
     
+    # For each value in the input list, if it is also in the needs list, return that number and the difference of 2020 and that number (the number in the needs set).
     for i in inputs:
         if i in needs:
             return i * (2020 - i)
@@ -24,11 +26,13 @@ def find_product3(inputs: List[int]) -> int:
     Find the three elements that add up to 2020 and return their product
     """
 
+    # Create a set of the result of 2020 minus each combination of input values.
     needs = {2020 - i  - j: (i, j)
             for i in inputs
             for j in inputs
             if i != j}
     
+    # For each input, if that input is in needs, j and k equal the previous i and j values. Return the product of the three numbers.
     for i in inputs:
         if i in needs:
             j, k = needs[i]
