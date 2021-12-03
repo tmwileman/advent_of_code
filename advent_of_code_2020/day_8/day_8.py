2a88702fb6dfb7679a1ff2ae17a5207422bcabab
twileman <twileman@redhat.com>
2021-12-03 11:19:00 -0500

    day 2 complete

--- advent_of_code_2020/day_8/day_8.py
+++ advent_of_code_2020/day_8/day_8.py
@@ -11,63 +11,69 @@ acc +1
 jmp -4
 acc +6"""
 
+
 class Instruction(NamedTuple):
     op: str
     arg: int
-    
+
     # Static method to parse each line into the operation and argument
     @staticmethod
     def parse(line: str) -> Instruction:
         op, arg = line.strip().split()
         return Instruction(op, int(arg))
 
+
 INSTRUCTIONS = [Instruction.parse(line) for line in RAW.split("\n")]
+
 
 class Booter:
     def __init__(self, instructions: List[Instruction]) -> None:
         self.instructions = instructions
         self.accumulator = 0
         self.idx = 0
-    
+
     # Logic for executing one instruction
     def execute_one(self) -> None:
         op, arg = self.instructions[self.idx]
-        if op == 'acc':
+        if op == "acc":
             self.accumulator += arg
             self.idx += 1
-        elif op == 'jmp':
+        elif op == "jmp":
             self.idx += arg
-        elif op == 'nop':
+        elif op == "nop":
             self.idx += 1
         else:
             raise ValueError(f"unknown op: {op}")
-     
-    # Run until you observe a repeat index number   
+
+    # Run until you observe a repeat index number
     def run_until_repeat(self) -> None:
         executed = set()
-        
+
         while self.idx not in executed:
             executed.add(self.idx)
             self.execute_one()
-    
+
     # If at the end of the instructions return True else False
     def does_terminate(self) -> bool:
         executed = set()
-        
+
         while self.idx not in executed:
             if self.idx == len(self.instructions):
                 return True
-            
+
             executed.add(self.idx)
             self.execute_one()
-        
+
         return False
-                 
+
+
 BOOTER = Booter(INSTRUCTIONS)
 BOOTER.run_until_repeat()
 assert BOOTER.accumulator == 5
 
-with open('/Users/thomaswileman/advent_of_code/advent_of_code_2020/day_8/input.txt') as f:
+with open(
+    "/Users/thomaswileman/advent_of_code/advent_of_code_2020/day_8/input.txt"
+) as f:
     raw = f.read()
 
 instructions = [Instruction.parse(line) for line in raw.split("\n")]
@@ -79,20 +85,21 @@ print(booter.accumulator)
 def find_terminator(instructions: List[Instruction]) -> int:
     for i, (op, arg) in enumerate(instructions):
         subbed = instructions[:]
-        
-        if op == 'nop':
-            subbed[i] = Instruction('jmp', arg)
-        elif op == 'jmp':
-            subbed[i] = Instruction('nop', arg)
+
+        if op == "nop":
+            subbed[i] = Instruction("jmp", arg)
+        elif op == "jmp":
+            subbed[i] = Instruction("nop", arg)
         else:
             continue
-        
+
         booter = Booter(subbed)
-        
+
         if booter.does_terminate():
             return booter.accumulator
-        
+
     raise RuntimeError("Never Terminated")
+
 
 assert find_terminator(INSTRUCTIONS) == 8
 
