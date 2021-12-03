775ee02ea4e20fa70b0c48541a16ea2fe46e1804
twileman <twileman@redhat.com>
2021-12-03 11:18:53 -0500

    day 2 complete

---
+++ advent_of_code_2021/day_2/day_2_part_2.py
@@ -0,0 +1,63 @@
+from dataclasses import dataclass
+from __future__ import annotations
+from typing import NamedTuple, List
+
+RAW = """forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2"""
+
+
+class Instruction(NamedTuple):
+    op: str
+    arg: int
+
+    # Static method to parse each line into the operation and argument
+    @staticmethod
+    def parse(line: str) -> Instruction:
+        op, arg = line.strip().split()
+        return Instruction(op, int(arg))
+
+
+@dataclass
+class Position:
+    x_pos: int = 0
+    y_pos: int = 0
+    aim: int = 0
+
+    def new_position(self, op: str, arg: int) -> None:
+        self.op = op
+        self.arg = arg
+
+        if op == "up":
+            self.aim = self.aim - arg
+        elif op == "down":
+            self.aim = self.aim + arg
+        elif op == "forward":
+            self.x_pos = self.x_pos + arg
+            self.y_pos = self.y_pos + self.aim * arg
+        else:
+            raise ValueError(f"unknown op: {op}")
+
+    def run_all_instructions(self, instructions: List[Instruction]) -> None:
+        for instruction in instructions:
+            self.new_position(instruction.op, instruction.arg)
+        return self.x_pos, self.y_pos
+
+
+INSTRUCTIONS = [Instruction.parse(line) for line in RAW.split("\n")]
+pos = Position()
+pos = pos.run_all_instructions(INSTRUCTIONS)
+assert (pos) == (15, 60)
+
+with open(
+    "/Users/thomaswileman/advent_of_code/advent_of_code_2021/day_2/input.txt"
+) as f:
+    raw = f.read()
+
+instructions = [Instruction.parse(line) for line in raw.split("\n")]
+pos = Position()
+print(pos.run_all_instructions(instructions))
+print(pos.x_pos * pos.y_pos)
